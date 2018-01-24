d15335625844bfbc7938bea8e7827239b27a43ed
Marco Lattuada <marco.lattuada@polimi.it>
2018-01-24 15:46:11 +0100

    WS docker is now started as daemon

--- WS/create_docker.sh
+++ WS/create_docker.sh
@@ -1,3 +1,3 @@
 docker rm opt-deadline-ws-run
 docker build -t opt-deadline-ws .
-docker run --name opt-deadline-ws-run -p 8080:8080 opt-deadline-ws
+docker run -d --name opt-deadline-ws-run -p 8080:8080 opt-deadline-ws
